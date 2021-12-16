d6ad149f47985852bba607ce0a73c4edaef4f5b2
Kaan Atmaca <katmaca2014@gmail.com>
2021-12-17 00:01:48 +0300

    hash vol1

--- client.py
+++ client.py
@@ -4,36 +4,17 @@ from Crypto.Hash import SHA3_256
 from Crypto import Random   # a bit better secure random number generation 
 import math
 import client_basics as cb
-
-def is_valid(self, p):
-    if p == self.zero: return True
-    l = (p.y ** 2) % self.q
-    r = ((p.x ** 3) + self.a * p.x + self.b) % self.q
-    return l == r
-
-def order(self, g):
-        """order of point g
-        >>> o = ec.order(g)
-        >>> assert ec.is_valid(a) and ec.mul(a, o) == ec.zero
-        >>> assert o <= ec.q
-        """
-        assert self.is_valid(g) and g != self.zero
-        for i in range(1, self.q + 1):
-            if self.mul(g, i) == self.zero:
-                return i
-            pass
-        raise Exception("Invalid order") 
-        pass
+import hashlib
 
 x = 93223115898197558905062012489877327981787036929201444813217704012422483432813
 y = 8985629203225767185464920094198364255740987346743912071843303975587695337619
 
 E = Curve.get_curve('secp256k1')
 
-point = Point(x,y,E)
-
-P = E.generator
+p = E.generator
 n = E.order
+
+point = Point(x,y,E)
 
 sa = random.randint(0, n-1) #sa is private key
 
@@ -45,3 +26,9 @@ k = random.randint(1, n-2)
 R = k * point
 r = R*x
 
+stuID = 28239
+msg = {'ID': stuID, 'H': h, 'S': s, 'IKPUB.X': ikpub.x, 'IKPUB.Y': ikpub.y}
+
+m = msg.len()
+
+h = hashlib.sha3_256(r//m)

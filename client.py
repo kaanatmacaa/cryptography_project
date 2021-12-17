09266ced38f56880b85f97706f93f3b8b0292f43
Kaan Atmaca <katmaca2014@gmail.com>
2021-12-17 12:30:51 +0300

    2.1 done

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
-import random 
 from ecpy.curves import Curve,Point
 from Crypto.Hash import SHA3_256
-from Crypto import Random   # a bit better secure random number generation 
+import Crypto.Random.random # a bit better secure random number generation 
 import math
 import client_basics as cb
 import hashlib
@@ -21,10 +20,10 @@ print("n: ", n)
 point = Point(x,y,E)
 print("point: ", point)
 
-sa = random.randint(0, n-1) #sa is private key
+sa = Crypto.Random.random.randint(0, n-1) #sa is private key
 print("sa: ", sa)
 
-qa = sa * point #qa is public key
+qa = sa * p #qa is public key
 print("qa: ",qa)
 
 #signature generation
@@ -32,18 +31,17 @@ stuID = stuID.to_bytes(2,byteorder="big")
 print("stuId: ", stuID)
 
 m = 28239
-k = random.randint(1, n-2)
+k = Crypto.Random.random.randint(1, n-2)
 
-R = k * point
-#r = R.x
+R = k * p
 r = (R.x) % n
-
 print("r:", r)
 
-rb = r.to_bytes(32,byteorder="big")
-mb = m.to_bytes(32,byteorder="big")
 
-h = SHA3_256.SHA3_256_Hash(rb+mb, True)
+r_byte = r.to_bytes(32, 'big')
+m_byte = m.to_bytes(2, 'big')
+
+h = SHA3_256.SHA3_256_Hash(r_byte+ m_byte, True)
 h = SHA3_256.SHA3_256_Hash.digest(h)
 h = int.from_bytes(h,"big")
 h = h % n
@@ -51,9 +49,16 @@ h = h % n
 s = (k- (sa*h)) 
 s = s % n
 
-msg = {'ID': stuID, 'H': h, 'S': s, 'IKPUB.X': x, 'IKPUB.Y': y}
-
 print("h: ", h)
 print("s: ", s)
-
-cb.IKRegReq(h,s,x,y)
+print(E.is_on_curve(qa))
+#cb.IKRegReq(h,s,qa.x,qa.y) ---------> ID: 28239 CODE: 106590
+"""
+ID: 28239 CODE: 106590
+Sending message is:  {'ID': 28239, 
+'H': 7483239667947657079221120183470408812468827778297638119224547277257303037189, 
+'S': 37072444792267534329042480053454078134844330208514188234371033536238826941057, 
+'IKPUB.X': 39976054850521507574967626301504309882077713530204759279452697237879489454571, 
+'IKPUB.Y': 42826606605638080211453913302126934486778992853270815969562555968218429004241}
+ID: 28239 CODE: 106590
+"""
